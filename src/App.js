bf3dda69797ff2f8bf1a66aaa271b9e7330a5f9d
Laurent <magrez0laurent@gmail.com>
2022-04-04 10:10:34 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from './component/Navbar.js';
+import Main from './component/Main.js';
+
+
+function App() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+
+  export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./stylesheets/main.css";
+//______________________________________________
+import App from './App.js';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+
