e6cbd6a2b991cf2eeacf18246a6ec9c3123fb73b
Caro-L-Dev <liardcar@gmail.com>
2022-04-07 11:38:49 +0200

    ..

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+// import {BrowserRouter, Routes, Route} from "react-router-dom";
 
+
+
+// import {NavBar} from "./components/NavBar.js";
+
+// import {Home} from "./Home.js";
+// import {Blog} from "./Blog.js";
+// import {Contact} from "./Contact.js";
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <NavBar />
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+//  export default App;
+
+import Home from "./pages/Home.jsx";
 import {Members} from "./pages/Members.js";
 
-import {NavBar} from "./components/NavBar.js";
+ function App() {
+   return (
+    <Home />
+   );
+ }
 
-import {Home} from "./Home.js";
-import {Blog} from "./Blog.js";
-import {Contact} from "./Contact.js";
-
-function App() {
-  return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="Blog" element={<Blog />} />
-        <Route path="Contact" element={<Contact />} />
-        <Route path="Members" element={<Members />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
+ export default App;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ import React from "react";
 // == Component
 const Hero = () => {
     return (
-        <div class="hero bg">
+        <div className="hero bg">
             <h1 className="hero__title">Foot 2 boue</h1>
             <div className="hero__btns">
                 <p>Prêt à en découdre ?</p>
