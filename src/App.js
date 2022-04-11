afc687b999b91cd11030ebe56d2cfc945db9a2a3
Laurent <magrez0laurent@gmail.com>
2022-04-11 09:18:10 +0200

    push member page change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
-import {Members} from "./pages/Members.js";
-
 import {NavBar} from "./components/NavBar.js";
-
 import Home from "./pages/Home.js";
-import {Blog} from "./pages/Blog.js";
-import {Contact} from "./pages/Contact.js";
+import {Members} from "./pages/Members.js";
 
 function App() {
   return (
@@ -14,8 +10,6 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="Blog" element={<Blog />} />
-        <Route path="Contact" element={<Contact />} />
         <Route path="Members" element={<Members />} />
       </Routes>
     </BrowserRouter>
