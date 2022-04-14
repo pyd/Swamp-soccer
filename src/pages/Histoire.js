4c5ee1367957ec0af81ac14707bb6db8eb345821
Kadiatou <kadidans@gmail.com>
2022-04-14 10:59:02 +0200

    ajouter les modification page histoire

--- src/pages/Histoire.js
+++ src/pages/Histoire.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 const Histoire = () => {
     return (
-        <div className= 'general'>
-            <h3 className='utile1'>HISTOIRE</h3>
-            <div className='titre1'>
-                <div className='photo1'>
+        <div className= 'general_histoire'>
+            <h3 className='histoire_titre'>HISTOIRE</h3>
+            <div className='histoire_descript'>
+                <div className='hitoire_img'>
                     <img src='./img/foot1.jpg' alt='foot1' />
                 </div>
-                <di className='histor'>
-                    <h3 className='utile2'>Le « swamp soccer » : un sport bien boueux </h3>
-                    <h4 className='histo'>Si certains évitent de tacheter leurs maillots de football, d’autres préfèrent se
+                <di className='histoire__descript'>
+                    <h3 className='descript_titre1'>Le « swamp soccer » : un sport bien boueux </h3>
+                    <h4 className='histoire_contenu'>Si certains évitent de tacheter leurs maillots de football, d’autres préfèrent se
                         jeter dans de la boue… Découvrons le « swamp soccer ».
                         Le « swamp soccer » ou « foot de boue » est une sorte de dérivée du football traditionnel
                         qui se jouedans la boue. Mais pourquoi se jetterait-on exprès dans
@@ -27,29 +27,29 @@ const Histoire = () => {
                     </h4>
                 </di>
             </div><br />
-            <div className='titre2'>
+            <div className='histoire_galerie'>
                 <div>
-                    <h3 className='utile2'>EQUIPES SWAMP SOCCER</h3>
+                    <h3 className='descript_titre1'>EQUIPES SWAMP SOCCER</h3>
                 </div>
                 <hr></hr>
-                <div className='photo'>
-                    <div className='galerie'>
+                <div className='histoire_photo'>
+                    <div className='galerie_photo'>
                         <a> <img className='image' src='./img/foot2.jpg' alt='foot2' />
                             <span className='spa'>Une photo d'équipe du swamp soccer prise en 2018 lors d'un match</span> </a>
                     </div>
-                    <div className='galerie'>
+                    <div className='galerie_photo'>
                         <a> <img className='image' src='./img/foot3.jpg' alt='foot3' />
                             <span className='spa'>Une photo d'équipe du swamp soccer prise en 2019 lors d'un match en Finlande</span></a>
                     </div>
-                    <div className='galerie'>
+                    <div className='galerie_photo'>
                         <a> <img className='image' src='./img/foot4.jpg' alt='foot4' />
                             <span className='spa'>Une photo d'équipe du swamp soccer prise en été 2020 lors d'un match</span> </a>
                     </div>
-                    <div className='galerie'>
+                    <div className='galerie_photo'>
                         <a> <img className='image' src='./img/foot5.jpg' alt='foot5' />
                             <span className='spa'>Une photo d'équipe du swamp soccer prise en 2021 lors d'un match dans 50cm de boue</span> </a>
                     </div>
-                    <div className='galerie'>
+                    <div className='galerie_photo'>
                         <a> <img className='image' src='./img/f.webp' alt='foot6' />
                             <span className='spa'>Une photo d'équipe du swamp soccer prise en 2021 lors d'un match</span></a>
                     </div>
