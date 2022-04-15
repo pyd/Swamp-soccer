9870f84637f91969d6a0e46441ec4f980beaa06e
Caro-L-Dev <liardcar@gmail.com>
2022-04-15 10:33:42 +0200

    style(pages/Contact): add info form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,6 @@
 import { FaFeatherAlt } from 'react-icons/fa';
 import React, { useState } from 'react';
+import { FaHome, FaPhoneAlt, FaPaperPlane  } from "react-icons/fa";
 
 function Contact() {
 
@@ -22,10 +23,10 @@ function Contact() {
   function blurFunction() {
     let parent = this.parentNode;
     // Let the "focus" class when the input is not empty even it lost focus
-    if(this.value == ""){
+    if (this.value == "") {
       parent.classList.remove("focus");
     }
-    
+
   }
 
 
@@ -50,8 +51,7 @@ function Contact() {
 
     //       <div className="coordonates">
     //         <div>Phone: +44(0)7774926245</div>
-    //         <div>Address:<br />Stewart Miller Swamp Soccer
-    //           Cumbrae view,<br />
+    //         <div>Address:<br />,<br />
     //           North Campbell Road,<br />
     //           Innellan,
     //           SbArgyll, PA23
@@ -92,7 +92,38 @@ function Contact() {
     <div className="contact">
       <div className="contact__container">
         <div className="contact__container__form">
-          <div className="contact__container__form--info"></div>
+          <div className="contact__container__form--info">
+            <h3 className="contact__info--title">Foot 2 boue</h3>
+            <p className="contact__info--text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+
+            <div className="contact__info__items">
+
+              <div className="contact__info__items--item">
+                < FaHome className="icon" />
+                <p>
+                  Stewart Miller Swamp Soccer
+                  Cumbrae view
+                </p>
+              </div>
+
+              <div className="contact__info__items--item">
+                < FaPhoneAlt className="icon" />
+                <p>
+                  06 34 67 98 12
+                </p>
+              </div>
+
+              <div className="contact__info__items--item">
+                < FaPaperPlane className="icon" />
+                <p>
+                  lorem@gmail.com
+                </p>
+              </div>
+
+            </div>
+          </div>
 
           <div className="contact__container__form--form">
 
